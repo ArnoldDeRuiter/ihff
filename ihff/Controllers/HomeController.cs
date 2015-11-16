04de7fd90e3dc049553ee7ec826604fe6b13b146
Arndroid94 <arnoldderuiter@gmail.com>
2015-11-16 20:13:13 +0100

    Initial setup
    Basis setup, om iedereen met GitHub te kunnen laten starten ook.

---
+++ ihff/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ihff.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Movies()
+        {
+            ViewBag.Message = "Movies...";
+
+            return View();
+        }
+
+        public ActionResult Food()
+        {
+            ViewBag.Message = "Food...";
+
+            return View();
+        }
+
+        public ActionResult Activities()
+        {
+            ViewBag.Message = "Activities...";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ihff/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ihff.Startup))]
+namespace ihff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
