f5572e78899200483fb935d7dab6352e43a373ea
Arndroid94 <arnoldderuiter@gmail.com>
2015-11-18 15:41:23 +0100

    Navbar met selected highlight
    Uitlijning en kleine uiterlijk fixjes die er niet meer helemaal tof
    waren sinds de vorige Commit.
    
    En nu kan je aan de Navbar zien waar je bent. (Ook op de mobiel)

---
+++ ihff/Scripts/navbar-highlight.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var ar = window.location.pathname.split("/");
+    var statement = ar[ar.length - 1];
+    statement = (ar == ",") ? "Home" : statement; //Ternary operator whoop
+    $("ul.navbar-nav li a:contains('" + statement + "')").css("color", "#ff0000").parent(".navbar-li").addClass("current");
+});
+
+
