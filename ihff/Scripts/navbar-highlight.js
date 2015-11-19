7eed13c211b5706a0508ac399135c83f5a91e1b0
WouterJanson <wouterjanson@icloud.com>
2015-11-19 23:30:25 +0100

    Juiste kleur in menu (#e4494f)

--- ihff/Scripts/navbar-highlight.js
+++ ihff/Scripts/navbar-highlight.js
@@ -2,7 +2,7 @@
     var ar = window.location.pathname.split("/");
     var statement = ar[ar.length - 1];
     statement = (ar == ",") ? "Home" : statement; //Ternary operator whoop
-    $("ul.navbar-nav li a:contains('" + statement + "')").css("color", "#ff0000").parent(".navbar-li").addClass("current");
+    $("ul.navbar-nav li a:contains('" + statement + "')").css("color", "#e4494f").parent(".navbar-li").addClass("current");
 });
 
 
