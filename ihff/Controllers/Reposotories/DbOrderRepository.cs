ca5ee7ecf92ff63bd5cc6f383b896881e1238438
Jeroenvdvalk <jeroen-gijs@hotmail.com>
2016-01-07 12:28:37 +0100

    Begin van DbOrder gemaakt en speial verwijderd
    DbOrderRepository en IOrderRepository aangemaakt. Hierin AddOrder
    toegevoegd voor het toevoegen van een order.
    
    Daarnaast ook nog de special verwijderd.

---
+++ ihff/Controllers/Reposotories/DbOrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ihff.Controllers.Reposotories;
+using ihff.Models;
+
+namespace ihff.Controllers.Reposotories
+{
+    public class DbOrderRepository : IOrderRepository
+    {
+        private IHFFdatabasecontext ctx = new IHFFdatabasecontext();
+
+        //Een order toevoegen aan de database.
+        public void AddOrder(float totalPrice, int amount, string wishlistCode, int itemId)
+        {
+            Order order = new Order(totalPrice, amount, wishlistCode, itemId);
+
+            ctx.Orderlines.Add(order);
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ ihff/Controllers/Reposotories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ihff.Controllers.Reposotories;
+using ihff.Models;
+
+namespace ihff.Controllers.Reposotories
+{
+    interface IOrderRepository
+    {
+        void AddOrder(float totalPrice, int amount, string wishlistCode, int itemId);
+    }
+}
