2b5d47ec59190bf85f69ce927594afad4fd5874a
WouterJanson <wouterjanson@icloud.com>
2016-01-08 02:03:21 +0100

    IMDB koppeling gemaakt
    Poster op detail pagina wordt nu geladen vanaf IMDB evenals dynamische
    link naar imdb pagina

---
+++ ihff/Controllers/Api/ImdbParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using ihff.Models;
+using Newtonsoft.Json.Linq;
+
+namespace ihff.Controllers.Api
+{
+    public class ImdbParser
+    {
+        public string GetImdbId(Item item)
+        {
+            var json = new WebClient().DownloadString("http://www.omdbapi.com/?t=" + item.Name + "&y=" + item.Year + "&plot=short&r=json");
+
+            JObject o = JObject.Parse(json);
+            string imdbID = (string)o.GetValue("imdbID");
+
+            return imdbID;
+        }
+
+        public string GetImdbPoster(Item item)
+        {
+            var json = new WebClient().DownloadString("http://www.omdbapi.com/?t=" + item.Name + "&y=" + item.Year + "&plot=short&r=json");
+
+            JObject o = JObject.Parse(json);
+            string imdbPoster = (string)o.GetValue("Poster");
+
+            return imdbPoster;
+        }
+    }
+}
