51afb9e7f24a5443c0cce94437e0b7563b7da77d
MrBamBam <mitchelvanbever@hotmail.com>
2016-01-14 17:07:48 +0100

    Model, reservation etc.
    Je moeder

---
+++ ihff/Models/OrderItemCombined.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ihff.Models
+{
+    public class OrderItemCombined
+    {
+        public Order Order { get; set; }
+        public Item Item { get; set; }
+    }
+}
