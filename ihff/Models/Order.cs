b7d145c1636e69919302062661fba887990f5dfe
WouterJanson <wouterjanson@icloud.com>
2016-01-14 12:28:48 +0100

    Order aangepast voor Food&Film

--- ihff/Models/Order.cs
+++ ihff/Models/Order.cs
@@ -16,6 +16,7 @@ namespace ihff.Models
         public double? TotalPrice { get; set; }
         public int Amount { get; set; }
         public int ItemId { get; set; }
+        public int ItemId2 { get; set; }
         public string WishlistCode { get; set; }
 
         //Sorry man
