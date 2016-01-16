63a83d2c72095c1e6ffdbcdcb58d98dc716c893e
Arndroid94 <arnoldderuiter@gmail.com>
2016-01-16 17:31:20 +0100

    Tussentijdje Wishlist Commit
    Er is weer veel gebeurt
    
    MVC maken van alles en nieuwe functionaliteit zoals Wishlist terug
    kunnen halen, en nieuwe wishUpdateOrder funcite knopje, maar nog niet
    functioneel.
    Dit ivm dat wishUpdateOrder als enige nog geMVC'd moet worden. :)

--- ihff/Controllers/WishlistController.cs
+++ ihff/Controllers/WishlistController.cs
@@ -19,57 +19,55 @@ namespace ihff.Controllers
         private IOrderItemRepository orderItem = new DbOrderItemRepository();
         //ja
         private IOrderRepository orderLine = new DbOrderRepository();
-
+        
         // GET: Wishlist
         public ActionResult Index()
         {
             List<OrderItemCombined> allCombined = new List<OrderItemCombined>();
-            //Code
-            
-            List<Item> sessionList = Session["wishList"] as List<Item>;
-            if (sessionList != null)
+            //List<Item> sessionList = Session["wishList"] as List<Item>; //Get from OrderLine where Code == Code, en dan de Items Get from where ItemId==ItemId            
+
+            if (Session["code"] != null)
             {
                 string Code = Session["code"].ToString();
-                var q = from x in sessionList
-                        group x by x.ItemId into g
-                        let count = g.Count()
-                        select new { Id = g.Key, Amount = count, All = from a in g select a };
 
-                foreach (var i in q)
+                List<Order> allOrders = orderItem.GetOrders(Code);
+
+                foreach (Order o in allOrders)
                 {
-                    var a = i.All.ToList()[0];
-                    double? prijsItem = ((double?)a.Price * (double?)i.Amount);
+                    Item it = orderItem.GetItem(o.ItemId);
 
                     OrderItemCombined combined = new OrderItemCombined();
                     //Order
-                    combined.ItemId = a.ItemId;
-                    combined.Amount = i.Amount;
-                    combined.TotalPrice = prijsItem;
-                    combined.WishlistCode = Code;
+                    combined.ItemId = o.ItemId;
+                    combined.Amount = o.Amount;
+                    combined.TotalPrice = o.TotalPrice;
+                    combined.WishlistCode = o.WishlistCode;
 
                     //Item
-                    combined.AgeClassification = a.AgeClassification;
-                    combined.Cast = a.Cast;
-                    combined.DescriptionENG = a.DescriptionENG;
-                    combined.DescriptionNL = a.DescriptionNL;
-                    combined.Director = a.Director;
-                    combined.Length = a.Length;
-                    combined.Year = a.Year;
-                    combined.DateBegin = a.DateBegin;
-                    combined.DateEnd = a.DateEnd;
-                    combined.EventType = a.EventType;
-                    combined.Image = a.Image;
-                    //combined.ItemId = a.ItemId;
-                    combined.MaxAvailabillity = a.MaxAvailabillity;
-                    combined.Name = a.Name;
-                    combined.Price = a.Price;
+                    combined.AgeClassification = it.AgeClassification;
+                    combined.Cast = it.Cast;
+                    combined.DescriptionENG = it.DescriptionENG;
+                    combined.DescriptionNL = it.DescriptionNL;
+                    combined.Director = it.Director;
+                    combined.Length = it.Length;
+                    combined.Year = it.Year;
+                    combined.DateBegin = it.DateBegin;
+                    combined.DateEnd = it.DateEnd;
+                    combined.EventType = it.EventType;
+                    combined.Image = it.Image;
+                    //combined.ItemId = it.ItemId;
+                    combined.MaxAvailabillity = it.MaxAvailabillity;
+                    combined.Name = it.Name;
+                    combined.Price = it.Price;
+
                     allCombined.Add(combined);
                 }
+
             }
             return PartialView(allCombined);
         }
                 
-        Models.Item selMovie;
+        
         [HttpPost]
         public ActionResult addWish(int id)
         {   
@@ -80,21 +78,70 @@ namespace ihff.Controllers
                 {
                     string code = wishlistRepository.getTempCode();
                     Session["code"] = code;
-                }
-                
-                //Items
-                selMovie = itemRepository.GetItem(id);
-                List<Item> addedItems = new List<Item>();
-                addedItems.Add(selMovie);
+                }                
+                string Code = Session["code"].ToString();
 
-                List<Item> wishList = new List<Item>();
-                wishList = (Session["wishList"]!=null) ? Session["wishList"] as List<Item> : wishList;
-                wishList.Add(selMovie);
-                Session["wishList"] = wishList;
+
+
+                //Wishlist Code
+                bool CodeAlready = false;
+
+                if (db.Wishlists.Any(wo => wo.WishlistCode == Code))
+                    CodeAlready = true;
+
+                if (!CodeAlready)
+                {
+                    Wishlist w = new Wishlist();
+                    w.WishlistCode = Code;
+                    db.Wishlists.Add(w);
+                    db.SaveChanges();
+                }
+
+
+                //Orderline bestaand
+                List<Order> allOrders = orderItem.GetOrders(Code);
+                 bool newItem = true;
+                foreach (Order o in allOrders)
+                {
+                    if (o.ItemId == id) {//jeroen? ItemId2
+                        newItem = false;
+                        db.Orderlines.Attach(o);
+                        db.Entry(o).State = System.Data.Entity.EntityState.Modified;
+
+                        Models.Item selItem = itemRepository.GetItem(id);
+
+                        o.ItemId = id;
+                        //o.ItemId2 = null; //jeroen?
+                        o.Amount = (o.Amount + 1);
+                        o.TotalPrice = (o.Amount * selItem.Price);
+                        o.WishlistCode = Code;
+
+                        db.Orderlines.Add(o);
+                        db.SaveChanges();
+                    }
+                }
+                //Orderline nieuw
+                if (newItem)
+                {                
+                    Models.Item selItem = itemRepository.GetItem(id);
+
+                    Order o = new Order();
+
+                    o.ItemId = id;
+                    //o.ItemId2 = null; //jeroen?
+                    int totAm = (o.Amount + 1);
+                    o.Amount = totAm;
+                    double? totPr = (o.Amount * selItem.Price);
+                    o.TotalPrice = totPr;
+                    o.WishlistCode = Code.ToString();
+
+                    db.Orderlines.Add(o);
+                    db.SaveChanges();
+                }
                 
                 return Redirect(Request.UrlReferrer.ToString());
             }
-            return View(selMovie);
+            return View();
         }
         
         public ActionResult wishUpdateOrder(int hiddenUpdateAmountVal, int hiddenUpdateIDVal)
@@ -117,7 +164,6 @@ namespace ihff.Controllers
                     count++;
                 }
             }
-
 
             if (amount == 0)
             {
@@ -156,10 +202,8 @@ namespace ihff.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-                                    //Code wishlist    //Name //ID  //COUNT //totPrijs
-        public ActionResult wishOrder(string Code, ihff.Models.OrderItemCombined  model) //, List<Tuple<string, int, int, double?>> lstTuple
-        {
-            List<Tuple<string, int, int, double?>> lstTuple = Session["tupleLijst"] as List<Tuple<string, int, int, double?>>;
+        public ActionResult wishOrder(string Code, FormCollection form)
+        {            
             if (ModelState.IsValid)
             {
                 bool CodeAlready = false;
@@ -173,24 +217,49 @@ namespace ihff.Controllers
                     db.Wishlists.Add(w);
                     db.SaveChanges();
                 }
-
-                foreach (var t in lstTuple)
+                
+                int Iteratie = (int.TryParse(form["Iteratie"], out Iteratie)) ? Convert.ToInt32(form["Iteratie"]) : 0;
+                for (int i = 1; i <= Iteratie; i++)
                 {
-                    int tID = t.Item2;
-                    double? tTotPrijs = t.Item4;
+                    int ItemId = (int.TryParse(form["ItemId_" + i], out ItemId)) ? Convert.ToInt32(form["ItemId_" + i]) : 0;
+                    int ItemId2 = (int.TryParse(form["ItemId2_" + i], out ItemId2)) ? Convert.ToInt32(form["ItemId2_" + i]) : 0;
+                    int Amount = (int.TryParse(form["Amount_" + i], out Amount)) ? Convert.ToInt32(form["Amount_" + i]) : 0;
+                    double TotalPrice = (double.TryParse(form["TotalPrice_" + i], out TotalPrice)) ? Convert.ToDouble(form["TotalPrice_" + i]) : 0.0;
+                    string WishlistCode = form["WishlistCode_" + i].ToString();
+
                     Order o = new Order();
-                    o.ItemId = tID;
-                    o.Amount = t.Item3;
-                    o.TotalPrice = tTotPrijs;
-                    o.WishlistCode = Code;
+                    o.ItemId = ItemId;
+                    o.ItemId2 = null; //jeroen? ItemId2
+                    o.Amount = Amount;
+                    o.TotalPrice = TotalPrice;
+                    o.WishlistCode = WishlistCode;
                     db.Orderlines.Add(o);
                 }
-                
+
                 db.SaveChanges();
                 return RedirectToAction("Index", "Reservation");
             }
             return View();
-        }       
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult wishReturningCode(string Code)
+        {
+            if (ModelState.IsValid)
+            {
+                bool CodeAlready = false;
+
+                if (db.Wishlists.Any(o => o.WishlistCode == Code))
+                    CodeAlready = true;
+
+                if (CodeAlready)
+                Session["code"] = Code;
+
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+            return View();   
+        }
 
         /*
         //terwijl dit v zou voldoen: moet er ^ gebeuren :p
