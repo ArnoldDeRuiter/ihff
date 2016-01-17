5158cd326d3efe43762847a0448df375b225f22f
Arndroid94 <arnoldderuiter@gmail.com>
2016-01-17 20:34:58 +0100

    Nog een klein geheim fixje
    Niet kijken. :D

--- ihff/Scripts/navbar-highlight.js
+++ ihff/Scripts/navbar-highlight.js
@@ -1,8 +1,14 @@
 ﻿$(document).ready(function () {
     var ar = window.location.pathname.split("/");
     var statement = ar[ar.length - 1];
+
+    statement = (statement == "FoodFilm") ? "Food & Film Ticket" : statement;
     statement = (ar == ",") ? "Home" : statement; //Ternary operator whoop
-    $("ul.navbar-nav li a:contains('" + statement + "')").css("color", "#e4494f").parent(".navbar-li").addClass("current");
+    //$("ul.navbar-nav li a:contains('" + statement + "')").css("color", "#e4494f").parent(".navbar-li").addClass("current");
+    $("ul.navbar-nav li a").filter(function () {
+        return $(this).text() === statement;
+    }).css("color", "#e4494f").parent(".navbar-li").addClass("current");
+
 });
 
 
